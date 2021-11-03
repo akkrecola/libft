7b757e98af7c13ea8dcce5aaea2d58191e7218ab
Erkka Lehtoranta <elehtora@hive.fi>
2021-11-03 19:10:30 +0200

    ++ isprint & appended .h

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/03 16:00:26 by elehtora          #+#    #+#             */
-/*   Updated: 2021/11/03 19:07:12 by elehtora         ###   ########.fr       */
+/*   Created: 2021/11/03 19:01:54 by elehtora          #+#    #+#             */
+/*   Updated: 2021/11/03 19:04:16 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int	ft_isprint(int c)
+{
+	if (32 <= c && c <= 126)
+		return (1);
+	else
+		return (0);
+}
 
-int	ft_islower(int c);
-int	ft_isupper(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
