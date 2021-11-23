cb18aaeba9aef61514111c4024a23ac8429b8d93
Erkka Lehtoranta <elehtora@hive.fi>
2021-11-23 23:24:50 +0200

    ++ atoi integer flow control (none :D) and random return (-1) on input longer than 19 digits

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:58:49 by elehtora          #+#    #+#             */
-/*   Updated: 2021/11/23 22:53:12 by elehtora         ###   ########.fr       */
+/*   Updated: 2021/11/23 23:38:48 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,18 @@ static const char	*set_sign(const char *nptr, int *sign)
 	return (nptr);
 }
 
+/*
+Integer overflow or underflow isn't handled on the actual atoi, therefore
+they're not handled here either. Atoi has a non-documented return of -1 when
+the count of digits passes 19, so this behaviour is implemented.
+*/
+
 int	ft_atoi(const char *nptr)
 {
 	int	sign;
 	int	digit;
 	int	collector;
+	int	i;
 
 	nptr = skip_whitespace(nptr);
 	sign = 1;
@@ -59,11 +66,14 @@ int	ft_atoi(const char *nptr)
 	if (!sign)
 		return (0);
 	collector = 0;
-	while (is_digit(*nptr))
+	i = 0;
+	while (is_digit(nptr[i]))
 	{
-		digit = *nptr - '0';
+		if (i > 18)
+			return (-1);
+		digit = nptr[i] - '0';
 		collector = collector * 10 + digit;
-		nptr++;
+		i++;
 	}
 	return (sign * collector);
 }
