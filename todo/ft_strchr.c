db98af600471f3d95f518475b0523ba65eb04424
Erkka Lehtoranta <erkka.lehtoranta@gmail.com>
2021-11-27 03:52:28 +0200

    ++ strchr

--- todo/ft_strchr.c
+++ todo/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 18:59:59 by elehtora          #+#    #+#             */
-/*   Updated: 2021/11/16 18:41:03 by elehtora         ###   ########.fr       */
+/*   Updated: 2021/11/27 03:51:54 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0')
+	while (*s != '\0' || c == '\0')
 	{
 		if (*s == c)
 			return ((char *) s);
