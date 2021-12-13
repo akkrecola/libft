71b6ee516a6be073e15981abe188e3e0eca59804
Erkka Lehtoranta <elehtora@hive.fi>
2021-12-13 23:35:09 +0200

    Modified memory function tests, left on strclr

--- tests/test_mem.c
+++ tests/test_mem.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 04:01:23 by elehtora          #+#    #+#             */
-/*   Updated: 2021/12/01 04:01:42 by elehtora         ###   ########.fr       */
+/*   Updated: 2021/12/13 23:33:42 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,35 @@
 TODO: memmove, memchr, memcpy
  */
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	char	*str;
 	size_t	size;
 
-	size = 10;
+	if (argc < 3)
+	{
+		ft_putstr("Too few arguments. (Usage: length of string, string)");
+		return (-1);
+	}
+	if (argc > 3)
+	{
+		ft_putstr("Too many arguments. (Usage: length of string, string)");
+		return (-1);
+	}
+
+	size = (size_t)ft_atoi(argv[1]);
 	str = (char *)ft_memalloc(size);
 	ft_memset(str, 'a', size);
+	ft_putstr("\nMemory area set, size of: ");
+	ft_putnbr(size);
+	ft_putstr("\nString: ");
 	ft_putstr(str);
-	ft_putnbr(ft_strlen(str));
+	ft_strclr(str);
+	ft_putstr("\nString cleared.");
 	ft_memdel((void **)&str);
-	ft_putstr(str);
-	ft_putstr(NULL);
+	ft_putstr("\nMemory area deleted.");
+	//ft_putstr(str);
+	//ft_putstr(NULL);
 	return (0);
 }
 
