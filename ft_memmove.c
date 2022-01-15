01723d2c70103d247ab44981046323c9f06a91bb
elehtora <elehtora@student.hive.fi>
2022-01-15 12:57:45 +0200

    ++memmove, working solution

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,20 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memdel.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/11 18:47:41 by elehtora          #+#    #+#             */
-/*   Updated: 2022/01/11 01:14:59 by elehtora         ###   ########.fr       */
+/*   Created: 2022/01/14 11:56:25 by elehtora          #+#    #+#             */
+/*   Updated: 2022/01/15 12:41:01 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 #include <string.h>
 
-void	ft_memdel(void **ap)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	free(*ap);
-	*ap = NULL;
+	void	*temp;
+
+	temp = ft_memalloc(n);
+	ft_memcpy(temp, src, n);
+	ft_memcpy(dest, temp, n);
+	ft_memdel(&temp);
+	return (dest);
 }
