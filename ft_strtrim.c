9a8bda46c76760b30ca11765ccdec1e0bf59e908
elehtora <elehtora@student.hive.fi>
2022-02-10 16:10:21 +0200

    Fixed strtrim (support for empty string)
    Strtrim segfaulted on an 'only whitespace' input. A condition
    was added to the get_tail -function to handle this case. The
    general clarity of the function was also somewhat improved.

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 13:09:05 by elehtora          #+#    #+#             */
-/*   Updated: 2022/01/10 01:51:41 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/02/10 16:08:21 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,35 @@ static int	is_whitespace(char c)
 		return (0);
 }
 
-static char	*find_tail(char const *s)
+static char	*get_tail(char const *s)
 {
-	while (*s != '\0')
-		s++;
-	while (is_whitespace(*s) || *s == '\0')
-		s--;
-	return ((char *)s);
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	if (i == 0)
+		return ((char *)s);
+	i--;
+	while (is_whitespace(s[i]))
+		i--;
+	return ((char *)&(s[i]));
 }
 
 char	*ft_strtrim(char const *s)
 {
 	char	*tail;
 	char	*trimmed;
-	char	*tstart;
+	char	*result_ptr;
 
 	while (is_whitespace(*s))
 		s++;
-	tail = find_tail(s);
-	trimmed = ft_strnew(ft_strlen(s));
-	tstart = trimmed;
+	tail = get_tail(s);
+	trimmed = (char *) malloc(ft_strlen(s) + 1);
+	if (trimmed == NULL)
+		return (NULL);
+	result_ptr = trimmed;
 	while (s != (tail + 1))
-	{
-		*trimmed = *s;
-		trimmed++;
-		s++;
-	}
-	trimmed = tstart;
-	return (trimmed);
+		*(trimmed++) = *(s++);
+	return (result_ptr);
 }
