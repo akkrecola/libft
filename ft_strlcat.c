fde773be34152f25beaf141ac4954bc491ca98f9
elehtora <elehtora@student.hive.fi>
2022-02-15 22:10:10 +0200

    WIP on strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 19:11:41 by elehtora          #+#    #+#             */
-/*   Updated: 2022/02/05 07:52:37 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/02/15 22:07:41 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,17 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	len;
+	size_t	i;
 
 	len = ft_strlen(dst) + ft_strlen(src);
-	while (*dst != '\0')
+	while (*dst != '\0' && size > 0)
 		dst++;
-	while (size-- > 0 && *src != '\0')
+	i = 0;
+	while (i++ < size && *src != '\0')
 		*(dst++) = *(src++);
-	*dst = '\0';
+	if (i == size && *src != '\0')
+		len = size;
+	else
+		*dst = '\0';
 	return (len);
 }
