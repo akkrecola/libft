192fc1b003a8451ee0b07d060fb5c80160efc30f
Erkka Lehtoranta <elehtora@hive.fi>
2022-02-18 03:57:02 +0200

    Cleaned strcat

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,25 +6,11 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 18:34:38 by elehtora          #+#    #+#             */
-/*   Updated: 2022/01/23 17:47:57 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/02/18 03:50:38 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
-static char	*ft_strcpy(char *dest, const char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+#include "libft.h"
 
 char	*ft_strcat(char *dest, const char *src)
 {
