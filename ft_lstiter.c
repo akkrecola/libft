5b89dd081ba8c5a0b523d1d762d15f34defd908d
Erkka Lehtoranta <elehtora@hive.fi>
2022-02-18 03:37:07 +0200

    Cleaned repo and removed unnecessary inclusions

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,12 +6,11 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 02:47:52 by elehtora          #+#    #+#             */
-/*   Updated: 2022/02/17 01:53:19 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/02/18 03:27:22 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
