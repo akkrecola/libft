d6e5da9c638ef06bb442bb043391a8de638717ef
Erkka Lehtoranta <elehtora@hive.fi>
2022-02-20 00:13:24 +0200

    Fixed strtrim allocation size

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 13:09:05 by elehtora          #+#    #+#             */
-/*   Updated: 2022/02/11 17:53:20 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/02/20 00:13:06 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ char	*ft_strtrim(char const *s)
 
 	start = get_start(s);
 	tail = get_tail(start);
-	trimmed = ft_strnew(tail - start + 2);
+	trimmed = ft_strnew(tail - start + 1);
 	if (trimmed == NULL)
 		return (NULL);
 	result_ptr = trimmed;
