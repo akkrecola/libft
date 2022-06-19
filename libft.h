a6d863db2c9e28f369a52279bd2a4d2fb96ee277
Erkka Lehtoranta <elehtora@hive.fi>
2022-06-19 15:54:06 +0300

    Restructured header by function 'class'

--- libft.h
+++ libft.h
@@ -6,14 +6,14 @@
 /*   By: elehtora <elehtora@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 16:00:26 by elehtora          #+#    #+#             */
-/*   Updated: 2022/06/18 20:09:25 by elehtora         ###   ########.fr       */
+/*   Updated: 2022/06/19 15:53:05 by elehtora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-/* Necessary (and permitted) inclusions */
+/* Inclusions */
 # include <string.h>
 
 /* Linked list typedef */
@@ -24,7 +24,20 @@ typedef struct s_list
 	struct s_list	*next;
 }				t_list;
 
-/* Part 1 function prototypes */
+// ASCII comparisons
+int		ft_islower(int c);
+int		ft_isupper(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_iswhite(int c);
+int		ft_arechars(char *str, const char *charset);
+
+// Memory manipulation
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
@@ -33,6 +46,14 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memrchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+
+// String functions
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+char	*ft_strnew(size_t size);
+void	ft_strdel(char **as);
 size_t	ft_strlen(const char *s);
 size_t	ft_strdlen(const char *s, const char delim);
 size_t	ft_strnlen(const char *s, size_t maxlen);
@@ -50,24 +71,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strpbrk(const char *s, const char *charset);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *str);
-int		ft_islower(int c);
-int		ft_isupper(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_iswhite(int c);
-int		ft_arechars(char *str, const char *charset);
-
-/* Part 2 function prototypes */
-void	*ft_memalloc(size_t size);
-void	ft_memdel(void **ap);
-char	*ft_strnew(size_t size);
-void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_strrclr(char *s);
 void	ft_striter(char *s, void (*f)(char *));
@@ -76,38 +79,35 @@ char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strsep(char **p_string, int c);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s);
 char	**ft_strsplit(char const *s, char c);
-char	*ft_itoa(int n);
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
+size_t	ft_count_digs(int n);
+
+// Output stream functions
 void	ft_putstr(char const *s);
 void	ft_putendl(char const *s);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putmem(char *s, size_t n);
 
-/* Bonus part function prototypes */
+// List functions
 t_list	*ft_lstnew(void const *content, size_t content_size);
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstadd(t_list **alst, t_list *new);
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-
-/* Mandatory helper function for list testing */
+t_list	*ft_lstcpyone(t_list *link);
 void	ft_del(void *content, size_t content_size);
 
-/* Voluntary functions (for testing) */
-size_t	ft_count_digs(int n);
-void	ft_putmem(char *s, size_t n);
-t_list	*ft_lstcpyone(t_list *link);
-
-/* Additional functions added after completion */
-char	*ft_strsep(char **p_string, int c);
+// Math functions
 size_t	ft_nat_sqrt(unsigned int n);
 
 #endif
